864a05c62f153ab00209bc7c58fdd9cb283b74ac
Beatriz Torres <btorre0@csu.fullerton.edu>
2025-04-13 17:09:53 -0700

    fixing dev setup, base routing w/ tailwind

---
+++ my-react-router-app/app/src/App.tsx
@@ -0,0 +1,17 @@
+// app/App.tsx
+
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// place holder:
+function Home() {
+  return <h2>Home Page</h2>;
+}
+
+function About() {
+  return <h2>About Page</h2>;}
+
+function App() {
+}
+
+export default App;
